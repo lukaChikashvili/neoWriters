f73d5b150170b916c91d6ec73c8e011ddd7a2f75
luka chikashvili <lukachikashvili2020@gmail.com>
2024-03-25 16:46:10 +0400

    add register form on home page

--- client/book/src/components/Home.jsx
+++ client/book/src/components/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import logo from '../assets/logo.png';
+import { Button, TextField } from '@mui/material';
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='flex items-center justify-around h-screen '>
+       <div>
+         <img src = {logo} />
+       </div>
+
+       <div className='w-1/3'>
+        <form className='w-full flex flex-col gap-6'>
+          <h1 className='text-4xl font-bold text-center'>რეგისტრაცია</h1>
+          <TextField label = "სახელი" variant='outlined' size="small" />
+        <TextField label = "გვარი" variant='outlined' size='small'/>
+        <TextField label = "ქალაქი" variant='outlined' size='small'/>
+        <TextField label = "პროფესია" variant='outlined' size="small" />
+        <TextField label = "პაროლი" variant='outlined' size='small' type = "password" />
+        <Button variant='contained' color = "success">რეგისტრაცია</Button>
+        </form>
+       </div>
     </div>
   )
 }
