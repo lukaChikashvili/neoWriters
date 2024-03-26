d81537514baf0cc78bc5d7b282bae749ed91ab15
luka chikashvili <lukachikashvili2020@gmail.com>
2024-03-26 16:02:10 +0400

    add protected route to profile component

--- client/book/src/App.js
+++ client/book/src/App.js
@@ -1,16 +1,28 @@
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
 import Profile from './components/Profile';
 
 
 function App() {
+  const isUserLoggedIn = localStorage.getItem('token');
+
   return (
     <div className="App">
       <Header />
        <Routes>
         <Route path = "/" element = {<Home />} />
-        <Route path = "/profile" element = {<Profile />} />
+   
+        <Route
+          path="/profile"
+          element={
+            isUserLoggedIn ? (
+              <Profile />
+            ) : (
+              <Navigate to="/" replace={true} />
+            )
+          }
+        />
        </Routes>
     </div>
   );
