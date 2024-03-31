a18432b36e18f76a8a4f7b225db73ef88282a0c8
luka chikashvili <lukachikashvili2020@gmail.com>
2024-03-31 14:22:04 +0400

    persisted my book data in localstorage

--- client/book/src/components/MyBooks.jsx
+++ client/book/src/components/MyBooks.jsx
@@ -9,38 +9,41 @@ const MyBooks = () => {
     const [myBook, setMyBook] = useState([]);
 
     useEffect(() => {
-      const name = localStorage.getItem('name');
-      if(name) {
-        const filterByName = books.filter(item => item.author.name === name);
-
-        setMyBook(filterByName);
-        localStorage.setItem('filteredByName', JSON.stringify(filterByName));
-      }
-      
- 
-    }, [books]);
-
+      initializeMyBook();
+    }, []);
+  
     useEffect(() => {
-       const data = localStorage.getItem('filteredByName');
-       setMyBook(JSON.parse(data));
-    }, [])
-
+      localStorage.setItem('myBook', JSON.stringify(myBook));
+    }, [myBook]);
+  
+    const initializeMyBook = () => {
+      const data = localStorage.getItem('myBook');
+      if (data) {
+        setMyBook(JSON.parse(data));
+      } else {
+        const name = localStorage.getItem('name');
+        if (name) {
+          const filterByName = books.filter(item => item.author.name === name);
+          setMyBook(filterByName);
+        }
+      }
+    };
     const deleteBook = async (id) => {
        await axiosInstance.delete(`http://localhost:4000/api/books/del/${id}`);
       setBooks(books.filter(item => item._id !== id));
-       console.log(books);
+      
     }
     
   return (
     <div className='flex flex-col'>
       <h1 className='text-4xl font-bold p-12'>ჩემი წიგნები</h1>
       <div className='flex items-center gap-12 p-12'>
-      {myBook.map((value) => (
+      {myBook.length === 0 ? <p className='text-2xl'>თქვენ არ გაქვთ წიგნები</p> : (myBook.map((value) => (
         <div className='flex flex-col gap-4' key={value._id}>
         <img src = {value.url} className='w-56 h-56 object-cover' />
         <Button variant='contained' color = "error" onClick={() => deleteBook(value._id)}>წაშლა</Button>
         </div>
-      ))}
+      )))}
        </div>
     </div>
   )
