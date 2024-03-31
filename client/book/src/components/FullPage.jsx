632e56300f1a30eb7a94b43aadcb7289290cb55e
luka chikashvili <lukachikashvili2020@gmail.com>
2024-03-31 18:16:05 +0400

    add currency to change

--- client/book/src/components/FullPage.jsx
+++ client/book/src/components/FullPage.jsx
@@ -56,12 +56,44 @@ const buyBook = () => {
    
 }
 
+const [showDollar, setShowDollar] = useState(false);
+const [showEuro, setShowEuro] = useState(false);
+const [showLari, setShowLari] = useState(false);
+
+const showTheLari = () => {
+    setShowDollar(false);
+    setShowEuro(false);
+    setShowLari(true);
+}
+
+const showTheDollar = () => {
+    setShowDollar(true);
+    setShowEuro(false);
+    setShowLari(true);
+}
+
+const showTheEuro = () => {
+    setShowDollar(false);
+    setShowEuro(true);
+    setShowLari(false);
+}
+
   return (
     <div className='p-24 px-56' >
-        {buyModal && <div className='w-4/5 h-4/5 mt-12 rounded-md shadow-lg bg-white border-4  border-green-500 absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-12'>
+        {buyModal && <div className='w-4/5 h-4/5 mt-12 rounded-md shadow-lg bg-white border-4  border-green-500 absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-12 flex flex-col gap-12'>
             <span className='absolute top-4 right-4 text-2xl font-semibold cursor-pointer' onClick={() => setBuyModal(false)}>X</span>
-               <h1 className='text-5xl font-bold'>ბალანსი: 5000</h1>
-
+               <h1 className='text-5xl font-bold'>ბალანსი: <span>{showDollar ? "185.53" : showEuro ? '171.78' : showLari ? '500' : ''}</span></h1>
+               <p className='text-2xl'>აირჩიეთ გადახდის მეთოდი: </p>
+               <div className='flex gap-12'>
+               <Button variant = "contained" color = "success" className='w-36' onClick={showTheLari}>ლარი</Button>
+               <Button variant = "contained" color = "error" className='w-36' onClick={showTheDollar} >დოლარი</Button>
+               <Button variant = "contained" color = "secondary" className='w-36' onClick={showTheEuro}>ევრო</Button>
+             
+                
+               </div>
+               {fullPage &&  (
+                    <img src = {fullPage.url} className='w-56 h-76 object-cover cursor-pointer shadow-lg rounded-md absolute right-56 top-8'/>
+                )}
             </div>}
 
 
@@ -83,7 +115,7 @@ const buyBook = () => {
             <p className='line pt-4 text-lg'>{fullPage.desc}</p>
             <div className='flex gap-8 '>
             <Button variant='outlined' color = "success" className = "w-56" onClick={() => addToCart(fullPage._id)}>კალათში დამატება</Button>
-        <Button variant='contained' color = "success"  className = "w-56" onClick={buyBook}>ყიდვა</Button>
+        <Button variant='contained' color = "success"  className = "w-56" onClick={() => buyBook(fullPage._id)}>ყიდვა</Button>
             </div>
             </div>
            
